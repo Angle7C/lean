433cb957d13dc5928210f57da5908b8471210c2e
chen <15305067103@163.com>
2022-08-27 14:47:11 +0800

    添加光线

--- src/RayTrack/ray.rs
+++ src/RayTrack/ray.rs
@@ -4,10 +4,14 @@ pub struct Ray{
     pub dir :vec4::Vec4,
 }
 impl Ray{
-    fn new(origin :vec4::Point3,direction:vec4::Vec4)->Ray{
-        Ray { orig: (origin), dir: (direction) }
+    pub fn new(origin :vec4::Point3,direction:vec4::Vec4)->Ray{
+        Ray { orig: (origin), dir: (direction.normalized()) }
     }
-    fn at(&self,t :f64)->Vec4{
+    pub fn at(&self,t :f64)->Vec4{
         self.orig+self.dir*t
+    }
+    pub fn ray_color(&self)->Vec4{
+        let t=0.5*(self.dir.y+1.0);
+        Vec4::new_vector(1.0, 1.0, 1.0)*(1.0-t)+Vec4::new_vector(0.4, 0.4, 1.0)*t
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,35 @@ fn main(){
 }
 #[cfg(test)]
 mod  test{
-    use crate::RayTrack::vec4;
+    use image::Pixel;
+
+    use crate::RayTrack::{vec4,ray};
     #[test]
     fn render_back(){
      const aspect_radio  : f64 = 16.0/9.0;
      const width:u32 =400;
      const height:u32=(width as f64/aspect_radio) as u32 ;
      
-     let imag = image::ImageBuffer::new(width, height);
-
+     let mut imag= image::RgbaImage::new(width, height);
      let viewport_h=2.0;
      let viewport_w=aspect_radio*viewport_h;
      let focal_length=1.0;
      let origin=vec4::Point3::new_point(0.0,0.0,0.0);
+     let horizontal =vec4::Vec4::new_vector(viewport_w, 0.0, 0.0);
+     let verital =vec4::Vec4::new_vector(0.0, viewport_h, 0.0);
+     let lower_left_corner=origin-horizontal/2.0-verital/2.0-vec4::Vec4::new_vector(0.0, 0.0, focal_length);
+     for i in 0..width{
+        for j in 0..height{
+            let u=i as f64/(width as f64-1.0);
+            let v=j as f64/(width as f64-1.0);
+            let ray =ray::Ray::new(origin, lower_left_corner+horizontal*u+verital*v-origin);
+            let color = ray.ray_color();
+            let pixel= imag.get_pixel_mut(i, j );
+            // println!("{:?}",color.rgba());
+            *pixel=color.rgba();
+            
+        }
+     }
+     imag.save("firstRender.png").unwrap();
     }
 }
