a4d1f16ba173a881d47173b11f8b183923350e16
chen <15305067103@163.com>
2022-08-31 17:02:09 +0800

    漫反射

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
-mod RayTrack;
-fn main(){
-    
-}
+mod Object;
+mod Raytrack;
+use Object::sphere::Sphere;
+fn main() {}
 #[cfg(test)]
-mod  test{
-    use image::Pixel;
-
-    use crate::RayTrack::{vec4,ray};
+mod test {
+    use crate::Object::sphere::Sphere;
+    use crate::Raytrack::ray::Ray;
+    use crate::Raytrack::vec3::Vec3;
+    use crate::Raytrack::hit_record::{Hittable, HittableList};
     #[test]
-    fn render_back(){
-     const aspect_radio  : f64 = 16.0/9.0;
-     const width:u32 =400;
-     const height:u32=(width as f64/aspect_radio) as u32 ;
-     
-     let mut imag= image::RgbaImage::new(width, height);
-     let viewport_h=2.0;
-     let viewport_w=aspect_radio*viewport_h;
-     let focal_length=1.0;
-     let origin=vec4::Point3::new_point(0.0,0.0,0.0);
-     let horizontal =vec4::Vec4::new_vector(viewport_w, 0.0, 0.0);
-     let verital =vec4::Vec4::new_vector(0.0, viewport_h, 0.0);
-     let lower_left_corner=origin-horizontal/2.0-verital/2.0-vec4::Vec4::new_vector(0.0, 0.0, focal_length);
-     for i in 0..width{
-        for j in 0..height{
-            let u=i as f64/(width as f64-1.0);
-            let v=j as f64/(width as f64-1.0);
-            let ray =ray::Ray::new(origin, lower_left_corner+horizontal*u+verital*v-origin);
-            let color = ray.ray_color();
-            let pixel= imag.get_pixel_mut(i, j );
-            // println!("{:?}",color.rgba());
-            *pixel=color.rgba();
-            
+    fn draw_sphere() {
+        let a=Sphere::new(Vec3::new(0.0, 100.0, -1.0), 99.5);
+        let b=Sphere::new(Vec3::new(0.0,0.0,  -1.0), 0.5);
+        let mut world=HittableList::new();
+        world.add(Box::new(b));
+
+        world.add(Box::new(a));
+
+        let width = 800;
+        let height = 800;
+        let mut image = image::RgbaImage::new(width, height);
+        for (_,_,item) in image.enumerate_pixels_mut(){
+            *item=image::Rgba([127,127,127,255])
         }
-     }
-     imag.save("firstRender.png").unwrap();
+
+        // return ;
+        let lower_left_corner=Vec3::new(-4.0, -2.0, -1.0);
+        let horizontal=Vec3::new(8.0, 0.0, 0.0);
+        let vertical=Vec3::new(0.0, 4.0, 0.0);
+        let origin=Vec3::new(0.0, 0.0, 0.0);
+        for i in 0..width {
+            for j in 0..height {
+                let u = i as f64 / (width-1) as f64;
+                let v = j as f64 / (height-1) as f64;
+                let  r= Ray::new(origin, lower_left_corner + horizontal*u + vertical*v-origin);
+                let pixel = image.get_pixel_mut(i, j);
+                for i in 0..100{
+                    *pixel=r.ray_color(&world).rgba();
+                }
+            }
+        }
+        image.save("sphere_test.png").unwrap();
+
     }
 }
